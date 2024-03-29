223ba905253722580cf0fb1c8ec4569595f5e309
Anish Mahapatra <anishmahapatra01@gmail.com>
2024-03-29 16:29:55 +0530

    Code to generate blogs on local using Llama 2

---
+++ llama2/bin/python.app
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PYTHONEXECUTABLE=/Users/anishmahapatra/Work/AI Projects/BlogGeneration_Llama2/llama2/bin/python
+/Users/anishmahapatra/Work/AI Projects/BlogGeneration_Llama2/llama2/python.app/Contents/MacOS/python "$@"
---
+++ llama2/lib/xsltConf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/anishmahapatra/Work/AI Projects/BlogGeneration_Llama2/llama2/lib"
+XSLT_LIBS="-lxslt -L/Users/anishmahapatra/Work/AI Projects/BlogGeneration_Llama2/llama2/lib -lxml2 "
+XSLT_PRIVATE_LIBS=""
+XSLT_INCLUDEDIR="-I/Users/anishmahapatra/Work/AI Projects/BlogGeneration_Llama2/llama2/include"
+MODULE_VERSION="xslt-1.1.37"
